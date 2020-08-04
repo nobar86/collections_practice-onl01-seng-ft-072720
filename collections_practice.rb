ae03982754660b404996d5dfee1091f16cb495bd
Michael Rabon <nobar86@gmail.com>
2020-08-04 19:26:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |left, right|
+    right <=> left
+end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |symbol|
+    symbol[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+  word[0] == "a"
+end
+end
+
+def sum_array(array)
+  array.inject do |sum,x|
+    sum + x 
+  end
+end
+
+def add_s(array)
+ array.each_with_index.collect do |word,index_number|
+  if index_number != 1 
+    word += "s"
+  else
+    word = word
+ end
+end
+end
